1bb49efb2ea7d90df4e046bfd86b581904bc6e23
Herbert Jordan <herbert@dps.uibk.ac.at>
2018-07-23 17:27:47 +0200

    Started implementing data fragment manager.

---
+++ code/prototype/include/allscale/runtime/data/data_item_manager.h
@@ -0,0 +1,18 @@
+/*
+ * The prototype implementation of the data item interface.
+ *
+ *  Created on: Jul 23, 2018
+ *      Author: herbert
+ */
+
+#pragma once
+
+namespace allscale {
+namespace runtime {
+namespace data {
+
+
+
+} // end of namespace com
+} // end of namespace runtime
+} // end of namespace allscale

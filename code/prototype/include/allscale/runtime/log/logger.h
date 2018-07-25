5a4281906e8249c1af817dd8aeb7cfc0cb727b58
Herbert Jordan <herbert@dps.uibk.ac.at>
2018-07-25 16:02:35 +0200

    Added debug logger.

---
+++ code/prototype/include/allscale/runtime/log/logger.h
@@ -0,0 +1,46 @@
+/*
+ * A simple logger utility to log events in the runtime.
+ *
+ *  Created on: Jul 25, 2018
+ *      Author: herbert
+ */
+
+#pragma once
+
+#include <mutex>
+
+#include <iostream>
+
+#include "allscale/runtime/com/node.h"
+
+namespace allscale {
+namespace runtime {
+namespace log {
+
+	// a switch to enable/disable logging
+	#ifndef ENABLE_LOGGING
+		#define ENABLE_LOGGING false
+	#endif
+
+	// the lock to sync logging messages
+	extern std::mutex g_log_lock;
+
+	// a utility to guard
+	struct log_guard {
+		std::mutex& m;
+		log_guard(std::mutex& m) : m(m) { if (ENABLE_LOGGING) m.lock(); };
+		~log_guard() { if (ENABLE_LOGGING) m.unlock(); }
+		operator bool() { return ENABLE_LOGGING; }
+	};
+
+	// a macro to log messages in a thread-save manor
+	#define DLOG if(allscale::runtime::log::log_guard g = allscale::runtime::log::g_log_lock) \
+		std::cout << "Node: " << allscale::runtime::com::Node::getLocalRank() << " - " << __FILE__ << ":" << __LINE__ << ": "
+
+
+} // end namespace log
+} // end namespace runtime
+} // end namespace allscale
+
+
+
---
+++ code/prototype/src/log/logger.cpp
@@ -0,0 +1,20 @@
+/*
+ * logger.cpp
+ *
+ *  Created on: Jul 25, 2018
+ *      Author: herbert
+ */
+
+
+#include "allscale/runtime/log/logger.h"
+
+
+namespace allscale {
+namespace runtime {
+namespace log {
+
+	std::mutex g_log_lock;
+
+} // end namespace log
+} // end namespace runtime
+} // end namespace allscale

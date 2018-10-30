d34fc648d52f021721b36f16de2feaa8ac784d05
Herbert Jordan <herbert@dps.uibk.ac.at>
2018-10-30 10:48:29 +0100

    MPI: fibers now conduct bussy waiting while waiting for a message to be send.

--- code/prototype/src/com/mpi/network.cpp
+++ code/prototype/src/com/mpi/network.cpp
@@ -219,9 +219,12 @@ namespace mpi {
 			return done == true;
 		};
 
+		// TODO: do not make fibers busy-wait here ... suspend them
+		auto fiber = allscale::utils::FiberPool::getCurrentFiber();
+
 		// wait for completion of send call
 		while(!done()) {
-			Network::getNetwork().processMessage();
+			if (!fiber) Network::getNetwork().processMessage();
 		}
 	}
 

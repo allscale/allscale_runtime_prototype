6431b984ccc7a80c3b3d68019e0d73bcec49d9cc
Herbert Jordan <herbert@dps.uibk.ac.at>
2019-01-16 15:32:28 +0100

    Added configuration option to force worker to core binding.

--- code/prototype/src/hw/model.cpp
+++ code/prototype/src/hw/model.cpp
@@ -8,9 +8,10 @@ namespace runtime {
 namespace hw {
 
 	const static char* ART_NUM_WORKERS = "ART_NUM_WORKERS";
+	const static char* ART_AFFINITY_FILL = "ART_AFFINITY_FILL";
 
 
-	std::vector<WorkerConfig> getWorkerPoolConfig(com::rank_t) {
+	std::vector<WorkerConfig> getWorkerPoolConfig(com::rank_t rank) {
 		std::vector<WorkerConfig> res;
 
 		int numWorkers = 1;
@@ -20,16 +21,32 @@ namespace hw {
 
 		numWorkers = std::max(numWorkers,1);
 
-		// create a mask allowing all workers to be active on all cores
-		cpu_set_t allCores;
-		CPU_ZERO(&allCores);
-		for(unsigned i=0; i<std::thread::hardware_concurrency(); i++) {
-			CPU_SET(i, &allCores);
-		}
+		// bind affinity if requested
+		if (std::getenv(ART_AFFINITY_FILL)) {
 
-		// create configurations
-		for(int i=0; i<numWorkers; i++) {
-			res.push_back({ allCores });
+			// crate configurations
+			for(int i=0; i<numWorkers; i++) {
+				cpu_set_t cores;
+				CPU_ZERO(&cores);
+				CPU_SET(rank*numWorkers+i,&cores);
+				res.push_back({ cores });
+			}
+
+		} else {
+
+
+			// create a mask allowing all workers to be active on all cores
+			cpu_set_t allCores;
+			CPU_ZERO(&allCores);
+			for(unsigned i=0; i<std::thread::hardware_concurrency(); i++) {
+				CPU_SET(i, &allCores);
+			}
+
+			// create configurations
+			for(int i=0; i<numWorkers; i++) {
+				res.push_back({ allCores });
+			}
+
 		}
 
 		// that's it
